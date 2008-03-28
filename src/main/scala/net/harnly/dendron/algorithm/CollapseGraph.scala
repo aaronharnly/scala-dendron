38a64b8ad2007918d19c385c6db8a1e47e0c1adf
Aaron Harnly <git@bulk.harnly.net>
2008-03-28 03:48:24 -0400

    Made CollapseGraph more friendly to generating a particular type

--- src/main/scala/net/harnly/dendron/algorithm/CollapseGraph.scala
+++ src/main/scala/net/harnly/dendron/algorithm/CollapseGraph.scala
@@ -1,17 +1,15 @@
 package net.harnly.dendron.algorithm
 
-abstract class CollapseGraph[V, E <: DirectedEdge[V], G <: DirectedGraph[V,E]]
-extends Function1[G,Graph[V,E]]
+abstract class CollapseGraph[V, E <: DirectedEdge[V], G <: DirectedGraph[V,E], H <: DirectedGraph[V,E]]
+extends GraphAlgorithm[V,E,G,H]
 {
-	def emptyGraph: G
+	def createGraph(v: Set[V], e: Set[E]): H
 	def shouldKeepVertex(vertex: V): Boolean
 	def createEdge(v1: V, v2: V): E
 
-	def apply(graph: G): Graph[V,E] = {
+	def apply(graph: G): H = {
 		val verticesToKeep = graph.vertices.filter(shouldKeepVertex(_))
 		val keepList = verticesToKeep.toList
-		
-		var newGraph = emptyGraph.addVertices(keepList : _*)
 		
 		def getFirstKeepersFrom(root: V): Set[V] = {
 			val outgoingEdges = graph.outgoingEdgesOf(root)
@@ -32,13 +30,14 @@ extends Function1[G,Graph[V,E]]
 
 		// from each keeper, find its first keeper neighbors.
 		// add an edge for each.
-		keepList.foldLeft(newGraph) { (g, keeper) =>
+		val edges = keepList.foldLeft(Set.empty[E]) { (set, keeper) =>
 			val targetsToAdd = getFirstKeepersFrom(keeper)
 			val edgesToAdd = targetsToAdd.map( t =>
 				createEdge(keeper, t)
 			)
-			g.addEdges(edgesToAdd.toList : _*)
+			set ++ edgesToAdd
 		}
+		createGraph(verticesToKeep, edges)
 	}
 	
 }
--- src/main/scala/net/harnly/dendron/algorithm/GraphAlgorithm.scala
+++ src/main/scala/net/harnly/dendron/algorithm/GraphAlgorithm.scala
@@ -1,4 +1,4 @@
 package net.harnly.dendron.algorithm
 
-trait GraphAlgorithm[V,E <: Edge[E],G <: Graph[V,E],B]
+trait GraphAlgorithm[V,E <: Edge[V],G <: Graph[V,E],B]
 extends Function1[G,B]
