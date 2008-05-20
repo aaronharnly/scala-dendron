eea7d6bc9c054db6d9d33202d5be002ac12796c6
Aaron Harnly <git@bulk.harnly.net>
2008-05-20 11:36:22 -0400

    Corrected partial application of indexed arrays to build a map

--- src/main/scala/net/harnly/dendron/datatypes/IndexedArray.scala
+++ src/main/scala/net/harnly/dendron/datatypes/IndexedArray.scala
@@ -6,5 +6,13 @@ case class IndexedArray2[K,V](array: Array[Array[V]], index: Map[K,Int])
 	lazy val length2 = array(0).length
 
 	def apply(k1: K, k2: K) = array( index(k1) )( index(k2) )
-	def apply(k1: K) = array(index(k1))
+	def apply(k1: K): Map[K,V] = Map( 
+		index.keys.toList.flatMap { k => 
+			val index2 = index(k)
+			if (index2 < length2)
+				Some( k -> apply(k1, k) )
+			else
+				None
+		} : _* 
+	) 
 }
--- src/test/scala/net/harnly/dendron/datatypes/IndexedArrayTests.scala
+++ src/test/scala/net/harnly/dendron/datatypes/IndexedArrayTests.scala
@@ -27,7 +27,7 @@ object indexedArrayTest extends Specification
 			indexed("c","a") must_== 4
 			indexed("c","b") must_== 5
 			
-			indexed("b").toList must_== List(2,3)
+			indexed("b") must_== Map( "a" -> 2, "b" -> 3)
 			
 		}
 	}
