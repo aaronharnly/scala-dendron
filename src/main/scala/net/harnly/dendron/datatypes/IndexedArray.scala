e3f0fc2f36825663eb84d633db229eae8f825725
Aaron Harnly <git@bulk.harnly.net>
2008-05-20 17:19:47 -0400

    Added a tabula for indexed arrays

--- src/main/scala/net/harnly/dendron/datatypes/IndexedArray.scala
+++ src/main/scala/net/harnly/dendron/datatypes/IndexedArray.scala
@@ -1,5 +1,7 @@
 package net.harnly.dendron.datatypes
 
+import net.harnly.tabula.Tabulae._
+
 case class IndexedArray2[K,V](array: Array[Array[V]], index: Map[K,Int])
 {
 	lazy val length1 = array.length
@@ -15,4 +17,63 @@ case class IndexedArray2[K,V](array: Array[Array[V]], index: Map[K,Int])
 				None
 		} : _* 
 	) 
+}
+
+// this thing kind of assumes square arrays
+class IndexedArrayTabula[K,V](
+	labeler: K => String,
+	finder: String => Option[K],
+	maker: Int => Array[Array[V]]
+)(
+	implicit valueTabula: Tabula[V,String]
+)
+extends Tabula[IndexedArray2[K,V],Iterable[String]]
+{
+	def encode(input: IndexedArray2[K,V]): Iterable[String] = {
+		val keysUnsorted = input.index.keys.toList
+		val keysSorted = keysUnsorted.sort( (a,b) =>
+			labeler(a) < labeler(b)
+		)
+		val labels = keysSorted.map(labeler)
+		val headerRow = "IndexedArray" + "\t" + labels.mkString("\t")
+
+		List(headerRow) ++ keysSorted.map( k_row =>
+			labeler(k_row) + "\t" + keysSorted.map( k_col => 
+				valueTabula.encode( input(k_row)(k_col) )
+			).mkString("\t")
+		)
+	}
+	
+	def decode(formatted: Iterable[String]): Result[IndexedArray2[K,V]] = {
+		val lineList = formatted.toList
+		val (firstLine, lines) = ((lineList.head.trim, lineList.tail))
+		val keysSorted: List[K] = firstLine.split("\t").toList.tail.flatMap(s => finder(s))
+		val index = Map( keysSorted.zipWithIndex : _* )
+		val reverseIndex = Map( index.map(p => ((p._2,p._1)) ).toList : _*)
+		val keysTotal = keysSorted.length
+		
+		val array = maker(keysTotal)
+		lines.foreach { line =>
+			val fields = line.trim.split("\t").toList
+			val (rowKeyString, valueFields) = ((fields.head, fields.tail))
+			val rowKeyOpt = finder(rowKeyString)
+			if (! rowKeyOpt.isDefined) 
+				return Left(new Exception("Unknown row key: " + rowKeyString))
+			val rowKey = rowKeyOpt.get
+
+			if (valueFields.length != keysTotal)
+				return Left(new Exception("Encountered row with " + valueFields.length + " entries instead of expected " + keysTotal + ": " + line))
+			
+			val valueResults = valueFields.map( v => valueTabula.decode(v) )
+			valueResults.toList.zipWithIndex.foreach { pair =>
+				val (vr, columnIndex) = pair
+				if (vr.isLeft)
+					return Left( vr.left.get )
+				val value = vr.right.get
+				array( index(rowKey) )( columnIndex ) = value
+			}
+		}
+		
+		Right(IndexedArray2(array, index))
+	}
 }
--- src/test/scala/net/harnly/dendron/datatypes/IndexedArrayTests.scala
+++ src/test/scala/net/harnly/dendron/datatypes/IndexedArrayTests.scala
@@ -9,26 +9,82 @@ object indexedArrayTest extends Specification
 {
 	"Indexed array 2" should {
 		"Retrieve properly" in {
-			val index = Map( "a" -> 0, "b" -> 1, "c" -> 2 )
-			val a = new Array[Array[Int]](3,2)
-			a(0)(0) = 0
-			a(0)(1) = 1
-			a(1)(0) = 2
-			a(1)(1) = 3
-			a(2)(0) = 4
-			a(2)(1) = 5
-			
-			val indexed = new IndexedArray2(a, index)
+			val indexed = ExampleIndexedArray.create
 			
 			indexed("a","a") must_== 0
 			indexed("a","b") must_== 1
-			indexed("b","a") must_== 2
-			indexed("b","b") must_== 3
-			indexed("c","a") must_== 4
-			indexed("c","b") must_== 5
+			indexed("b","a") must_== 3
+			indexed("b","b") must_== 4
+			indexed("c","a") must_== 6
+			indexed("c","b") must_== 7
+			indexed("c","c") must_== 8
 			
-			indexed("b") must_== Map( "a" -> 2, "b" -> 3)
+			indexed("b") must_== Map( "a" -> 3, "b" -> 4, "c" -> 5)
 			
 		}
+		
+		"Encode" in {
+			import net.harnly.tabula.Tabulae._
+			import net.harnly.tabula.StringPrimitiveTabulae._
+
+			val indexed = ExampleIndexedArray.create
+			implicit val tabula: Tabula[IndexedArray2[String,Int],Iterable[String]] = new IndexedArrayTabula[String,Int](
+				identity,
+				{k: String => Some(k)},
+				{size: Int => new Array[Array[Int]](size,size)}
+			)
+			indexed.encodeAs[Iterable[String]].toList must_== List(
+				"IndexedArray	a	b	c",
+				"a	0	1	2",
+				"b	3	4	5",
+				"c	6	7	8"
+			)
+		}
+		
+		"Decode" in {
+			import net.harnly.tabula.Tabulae._
+			import net.harnly.tabula.StringPrimitiveTabulae._
+			implicit val tabula: Tabula[IndexedArray2[String,Int],Iterable[String]]  = new IndexedArrayTabula[String,Int](
+				identity,
+				{k: String => Some(k)},
+				{size: Int => new Array[Array[Int]](size,size)}
+			)
+
+			val text: Iterable[String] = """IndexedArray	a	b	c
+a	0	1	2
+b	3	4	5
+c	6	7	8
+""".split("\n")
+			val indexedResult = text.decodeAs[IndexedArray2[String,Int]]
+			indexedResult.isRight must_== true
+			val indexed = indexedResult.right.get
+			
+			indexed("a","a") must_== 0
+			indexed("a","b") must_== 1
+			indexed("b","a") must_== 3
+			indexed("b","b") must_== 4
+			indexed("c","a") must_== 6
+			indexed("c","b") must_== 7
+			indexed("c","c") must_== 8
+		}
+	}
+}
+
+object ExampleIndexedArray
+{
+	def create: IndexedArray2[String,Int] = {
+		val index = Map( "a" -> 0, "b" -> 1, "c" -> 2 )
+		val a = new Array[Array[Int]](3,3)
+		a(0)(0) = 0
+		a(0)(1) = 1
+		a(0)(2) = 2
+		a(1)(0) = 3
+		a(1)(1) = 4
+		a(1)(2) = 5
+		a(2)(0) = 6
+		a(2)(1) = 7
+		a(2)(2) = 8
+		
+		IndexedArray2(a, index)
 	}
 }
