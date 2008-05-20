b5538ba4f5c8b3b08c579df2ea3dfe8c2c3fd5cd
Aaron Harnly <git@bulk.harnly.net>
2008-05-20 11:15:43 -0400

    Added partial application to get back rows of indexedarrays

--- src/main/scala/net/harnly/dendron/datatypes/IndexedArray.scala
+++ src/main/scala/net/harnly/dendron/datatypes/IndexedArray.scala
@@ -5,5 +5,6 @@ case class IndexedArray2[K,V](array: Array[Array[V]], index: Map[K,Int])
 	lazy val length1 = array.length
 	lazy val length2 = array(0).length
 
-	def apply(x1: K, x2: K) = array( index(x1) )( index(x2) )
+	def apply(k1: K, k2: K) = array( index(x1) )( index(x2) )
+	def apply(k1: K) = array(index(k1))
 }
